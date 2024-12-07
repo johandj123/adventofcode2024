c6189f19baeadac0648654bb70aa1c6bd6889d34
Johan de Jong <johan.de.jong@sogeti.com>
2024-12-07 13:27:22 +0100

    Day 7 with CartesianProductUtil.

--- src/Day7.java
+++ src/Day7.java
@@ -1,15 +1,22 @@
+import lib.CartesianProductUtil;
 import lib.InputUtil;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.function.BiFunction;
 
 public class Day7 {
     public static void main(String[] args) throws IOException {
         List<Line> lines = InputUtil.readAsLines("input7.txt").stream()
                 .map(Line::new)
                 .toList();
-        System.out.println(lines.stream().filter(Line::check).mapToLong(l -> l.test).sum());
-        System.out.println(lines.stream().filter(Line::check2).mapToLong(l -> l.test).sum());
+        System.out.println(lines.stream().filter(x -> x.check(List.of(
+                Long::sum, (a, b) -> a * b
+        ))).mapToLong(l -> l.test).sum());
+
+        System.out.println(lines.stream().filter(x -> x.check(List.of(
+                Long::sum, (a, b) -> a * b, (a, b) -> Long.parseLong(a + Long.toString(b))
+        ))).mapToLong(l -> l.test).sum());
     }
 
     static class Line {
@@ -30,46 +37,16 @@ public class Day7 {
                     '}';
         }
 
-        public boolean check() {
-            for (int i = 0; i < (1 << (values.size() - 1)); i++) {
-                long cur = values.get(0);
-                for (int j = 0; j < values.size() - 1; j++) {
-                    int maskval = i & (1 << j);
-                    if (maskval == 0) {
-                        cur = cur + values.get(j + 1);
-                    } else {
-                        cur = cur * values.get(j + 1);
-                    }
-                }
-                if (cur == test) {
-                    return true;
-                }
-            }
-            return false;
+        public boolean check(List<BiFunction<Long, Long, Long>> ops) {
+            return CartesianProductUtil.cartesianProductStream(values.size() - 1, ops).anyMatch(this::test);
         }
 
-        public boolean check2() {
-            outer:
-            for (int i = 0; i < (1 << (2 * (values.size() - 1))); i++) {
-                long cur = values.get(0);
-                for (int j = 0; j < values.size() - 1; j++) {
-                    int maskval = (i >> (2 * j)) & 3;
-                    long next = values.get(j + 1);
-                    if (maskval == 0) {
-                        cur = cur + next;
-                    } else if (maskval == 1) {
-                        cur = cur * next;
-                    } else if (maskval == 2) {
-                        cur = Long.parseLong(cur + Long.toString(next));
-                    } else {
-                        continue outer;
-                    }
-                }
-                if (cur == test) {
-                    return true;
-                }
+        private boolean test(List<BiFunction<Long, Long, Long>> l) {
+            long cur = values.get(0);
+            for (int j = 0; j < values.size() - 1; j++) {
+                cur = l.get(j).apply(cur, (long) values.get(j + 1));
             }
-            return false;
+            return cur == test;
         }
     }
 }
