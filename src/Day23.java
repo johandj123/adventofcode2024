ffe3c3e81e8c70487b52abbebcf1ff8a65b67d29
Johan de Jong <johan.de.jong@sogeti.com>
2024-12-23 12:02:13 +0100

    Moved the Bron-Kerbosch algorithm to the Graph library class.

--- src/Day23.java
+++ src/Day23.java
@@ -21,43 +21,6 @@ public class Day23 {
         second(graph);
     }
 
-    private static void second(Graph<String> graph) {
-        Set<Set<String>> cliques = BronKerbosch2(graph);
-        int largest = cliques.stream().mapToInt(Set::size).max().orElseThrow();
-        List<String> list = cliques.stream()
-                .filter(s -> s.size() == largest)
-                .map(ArrayList::new)
-                .map(s -> s.stream().sorted().collect(Collectors.joining(",")))
-                .toList();
-        list.forEach(System.out::println);
-    }
-
-    private static Set<Set<String>> BronKerbosch2(Graph<String> graph) {
-        Set<Set<String>> cliques = new HashSet<>();
-        BronKerbosch2(graph, new HashSet<>(), new HashSet<>(graph.getNodes()), new HashSet<>(), cliques);
-        return cliques;
-    }
-
-    private static void BronKerbosch2(Graph<String> graph, Set<String> R, Set<String> P, Set<String> X, Set<Set<String>> cliques) {
-        if (P.isEmpty() && X.isEmpty()) {
-            cliques.add(R);
-            return;
-        }
-        String u = !P.isEmpty() ? P.iterator().next() : X.iterator().next();
-        Set<String> Pu = new HashSet<>(P);
-        Pu.removeAll(graph.getNeighbours(u));
-        for (String v : Pu) {
-            Set<String> R2 = new HashSet<>(R);
-            R2.add(v);
-            Set<String> n = graph.getNeighbours(v);
-            Set<String> P2 = P.stream().filter(n::contains).collect(Collectors.toSet());
-            Set<String> X2 = X.stream().filter(n::contains).collect(Collectors.toSet());
-            BronKerbosch2(graph, R2, P2, X2, cliques);
-            P.add(v);
-            X.remove(v);
-        }
-    }
-
     private static void first(Graph<String> graph) {
         Set<Set<String>> threeSets = new HashSet<>();
         for (String a : graph.getNodes()) {
@@ -80,5 +43,16 @@ public class Day23 {
             }
         }
         System.out.println(count);
+    }
+
+    private static void second(Graph<String> graph) {
+        Set<Set<String>> cliques = graph.maxCliques();
+        int largest = cliques.stream().mapToInt(Set::size).max().orElseThrow();
+        List<String> list = cliques.stream()
+                .filter(clique -> clique.size() == largest)
+                .map(ArrayList::new)
+                .map(s -> s.stream().sorted().collect(Collectors.joining(",")))
+                .toList();
+        list.forEach(System.out::println);
     }
 }
